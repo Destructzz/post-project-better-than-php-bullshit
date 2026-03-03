936386a29a2396a22504f01390d899b120dd597d
Destructzz <sklyarenko_2006@mail.ru>
2026-03-03 18:27:01 +0300

    fix post

--- backend/src/post/post.controller.ts
+++ backend/src/post/post.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post as HttpPost, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
@@ -10,7 +10,7 @@ import { Roles } from '../auth/roles.decorator';
 export class PostController {
   constructor(private readonly postService: PostService) { }
 
-  @HttpPost()
+  @Post()
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('admin')
   create(@Body() createPostDto: CreatePostDto) {
